b1a6445020cf18d136dd9d6a8529789e7cd98407
chense <chense@wix.com>
2024-09-18 11:03:48 +0300

    change settings form layout and add widget preview

--- src/components/settings-form.tsx
+++ src/components/settings-form.tsx
@@ -1,12 +1,14 @@
 import React, { type FC } from 'react';
 import {
-    Box, Dropdown,
+    Box, Cell, Dropdown,
     FormField,
     Input,
+    Layout,
     NumberInput
 } from '@wix/design-system';
 import {CalculationMethod, EligibleItems, Settings} from '../types';
 import '@wix/design-system/styles.global.css';
+import { PluginPreview } from './plugin-preview';
 
 type Props = {
   settings: Settings;
@@ -22,71 +24,76 @@ export const SettingsForm: FC<Props> = ({
     const amountPrefix = settings.calculationMethod === CalculationMethod.FIXED ? '$' : '%';
 
     return (
-    <Box gap={3} direction="vertical">
-      <Box gap={3} direction="vertical" width={'100%'}>
-          <FormField
-              required
-              label='What items should insurance be collected for?'
-          >
-              <Dropdown
-                    placeholder='Select items'
-                    initialSelectedId={EligibleItems.ALL}
-                    options={[
-                        { id: EligibleItems.ALL, value: 'All' },
-                        { id: EligibleItems.SHIPPABLE, value: 'Only shippable items' },
-                    ]}
-                    onSelect={(option) => setSettings({
-                        ...settings,
-                        eligibleItems: option.id as EligibleItems,
-                    })}
-              />
-          </FormField>
-          <FormField
-              required
-              label='Insurance calculation:'
-          >
-              <Dropdown
-                  placeholder='Insurance claculation'
-                  initialSelectedId={CalculationMethod.FIXED}
-                  options={[
-                      { id: CalculationMethod.FIXED, value: 'FIXED' },
-                      { id: CalculationMethod.PERCENTAGE_FROM_TOTAL, value: 'PERCENTAGE_FROM_TOTAL' },
-                      { id: CalculationMethod.PERCENTAGE_FROM_SHIPPING_COST, value: 'PERCENTAGE_FROM_SHIPPING_COST' },
-                  ]}
-                  onSelect={(option) => setSettings({
-                      ...settings,
-                      calculationMethod: option.id as CalculationMethod,
-                  })}
-              />
-          </FormField>
-          <FormField
-              required
-              label= {amountTitle}
-          >
-                <NumberInput
-                    value={settings.amount}
-                    prefix={<Input.Affix value={amountPrefix} />}
-                    min={1}
-                    onChange={(val) => setSettings({
-                        ...settings,
-                        amount: val ?? 1,
-                    })}
-                />
-          </FormField>
-        <FormField
-          required
-          label='Explain to your users about the Insurance'
-        >
-          <Input
-            value={settings.description}
-            placeholder='description'
-            onChange={(val) => setSettings({
-              ...settings,
-              title: val.target.value
-            })}
-          />
-        </FormField>
-      </Box>
-    </Box>
+    <Layout>
+        <Cell span={6}>
+            <Box gap={3} direction="vertical">
+                <FormField
+                    required
+                    label='What items should insurance be collected for?'
+                >
+                    <Dropdown
+                        placeholder='Select items'
+                        options={[
+                            { id: EligibleItems.ALL, value: 'All' },
+                            { id: EligibleItems.SHIPPABLE, value: 'Only shippable items' },
+                        ]}
+                        initialSelectedId={EligibleItems.ALL}
+                        onSelect={(option) => setSettings({
+                            ...settings,
+                            eligibleItems: option.id as EligibleItems,
+                        })}
+                    />
+                </FormField>
+                <FormField
+                    required
+                    label='Insurance calculation:'
+                >
+                    <Dropdown
+                        placeholder='Insurance claculation'
+                        options={[
+                            { id: CalculationMethod.FIXED, value: 'FIXED' },
+                            { id: CalculationMethod.PERCENTAGE_FROM_TOTAL, value: 'PERCENTAGE_FROM_TOTAL' },
+                            { id: CalculationMethod.PERCENTAGE_FROM_SHIPPING_COST, value: 'PERCENTAGE_FROM_SHIPPING_COST' },
+                        ]}
+                        initialSelectedId={CalculationMethod.FIXED}
+                        onSelect={(option) => setSettings({
+                            ...settings,
+                            calculationMethod: option.id as CalculationMethod,
+                        })}
+                    />
+                </FormField>
+                <FormField
+                    required
+                    label= {amountTitle}
+                >
+                    <NumberInput
+                        value={settings.amount}
+                        prefix={<Input.Affix value={amountPrefix} />}
+                        min={1}
+                        onChange={(val) => setSettings({
+                            ...settings,
+                            amount: val ?? 1,
+                        })}
+                    />
+                </FormField>
+                <FormField
+                    required
+                    label='Explain to your users about the Insurance'
+                >
+                    <Input
+                        value={settings.description}
+                        placeholder='description'
+                        onChange={(val) => setSettings({
+                            ...settings,
+                            title: val.target.value
+                        })}
+                    />
+                </FormField>
+            </Box>
+        </Cell>
+        <Cell span={6}>
+            <PluginPreview {...settings}/>
+        </Cell>
+    </Layout>
   );
 };
